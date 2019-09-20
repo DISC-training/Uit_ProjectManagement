f9b2e86d4259bffccd92d9499108c912f1b20a63
phuctrung0201 <mygalaxy2712@gmail.com>
2019-09-20 22:35:22 +0700

    Initial project management

---
+++ README.md
@@ -0,0 +1,12 @@
+#ProjectManage
+###Use for managing disc-team's studying
+
+
+1. Install package for dev
+```
+npm run import
+```
+2. Run serve side(port: 5000) and client side(port:3000)
+```
+npm run start
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.get('/api',(req,res)=>{
+    res.json(
+        [
+            {name:'Tony Stark',char:'Iron Man'},
+        ]
+    )
+})
+app.get('*', (req, res) => {
+    
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
